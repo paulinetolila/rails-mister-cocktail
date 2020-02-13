364c97e29df7461f80ca6ba8f813033902a118d5
Pauline Tolila <pauline.tolila@gmail.com>
2020-02-13 16:59:46 +0100

    mister cocktail on-going

---
+++ app/models/dose.rb
@@ -0,0 +1,8 @@
+class Dose < ApplicationRecord
+  belongs_to :cocktail
+  belongs_to :ingredient
+  validates :description, :cocktail, :ingredient, presence: true
+  validates :ingredient, uniqueness: {
+    scope: :cocktail
+  }
+end
