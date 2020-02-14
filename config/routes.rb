9b9077e5337bdb97373a986ebd292ad06f5cf3c9
Pauline Tolila <pauline.tolila@gmail.com>
2020-02-14 15:13:29 +0100

    delete cocktail

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'cocktails#index'
-  resources :cocktails, only: [:show, :new, :create, :delete] do
+  resources :cocktails, only: [:show, :new, :create, :destroy] do
     resources :doses, only: [:new, :create]
   end
   resources :doses, only: [:destroy]
