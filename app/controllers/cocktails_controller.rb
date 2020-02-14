3f8c73b0af2919850982b49e06f41ab9ec5cf5ac
Pauline Tolila <pauline.tolila@gmail.com>
2020-02-14 15:10:19 +0100

    delete cocktail

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -22,6 +22,11 @@ class CocktailsController < ApplicationController
     end
   end
 
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+    redirect_to root_path
+  end
   private
 
   def cocktail_params
